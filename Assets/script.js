ccb9e0b4897dca6c8b2662eb56f76dc2554edf03
Rashad Anderson <kuminachi121@gmail.com>
2022-01-05 22:21:47 -0500

    added questions 2 & 3

--- Assets/script.js
+++ Assets/script.js
@@ -10,3 +10,28 @@ startButton.addEventListener("click", function () {
     startQuestionsEl.classList.remove("hide")
 })
 
+var ansButton = document.getElementById("answer-btn")
+
+ansButton.addEventListener("click", function (){
+    var choiceButtonEl = document.querySelector(".questions")
+
+    choiceButtonEl.classList.add("hide")
+
+    var nextQuestionEl = document.querySelector(".question-2")
+
+    nextQuestionEl.classList.remove("hide")
+})
+
+var ansButton2 = document.getElementById("answer-btn2")
+
+ansButton2.addEventListener("click", function(){
+    var choiceButtonEl = document.querySelector(".question-2")
+
+    choiceButtonEl.classList.add("hide")
+
+    var ansButton3El = document.querySelector(".question-3")
+
+    ansButton3El.classList.remove("hide")
+})
+
+
